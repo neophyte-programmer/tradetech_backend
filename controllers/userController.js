160652a300fd0c6c62a9247523b804079ecd2b95
Nutifafa Afi Attor <attorfafa@gmail.com>
2023-06-19 08:27:00 +0000

    get all users

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,7 @@ const { generateToken } = require('../config/jwtToken');
 const User = require('../models/userModel');
 const asyncHandler = require('express-async-handler');
 
+// Register Controller
 const createUser = asyncHandler(async (req, res) => {
     // get the email from the request body
     const email = req.body.email;
@@ -21,7 +22,7 @@ const loginUser = asyncHandler(async (req, res) => {
     const { email, password } = req.body;
     // find the user with the email
     const findUser = await User.findOne({ email: email })
-    
+
     if (findUser && await findUser.isPasswordMatched(password)) {
         res.json({
             _id: findUser._id,
@@ -36,6 +37,28 @@ const loginUser = asyncHandler(async (req, res) => {
     }
 })
 
+// Fetch all users
+const getAllUsers = asyncHandler(async (req, res) => {
+    try {
+        const getUsers = await User.find()
+        res.json(getUsers)
+    } catch (error) {
+        throw new Error(error)
+    }
+})
+
+// Fetch a single user
+const getSingleUser = asyncHandler(async (req, res) => {
+
+})
+
+// Update a user
+const updateUser = asyncHandler(async (req, res) => {
+
+})
+
+
+
 module.exports = {
-    createUser, loginUser
+    createUser, loginUser, getAllUsers, getSingleUser, updateUser
 }
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { createUser, loginUser } = require('../controllers/userController');
+const { createUser, loginUser, getAllUsers } = require('../controllers/userController');
 const router = express.Router();
 
 
 
 router.post('/register', createUser)
-router.post('/login', loginUser )
+router.post('/login', loginUser)
+router.get('/all', getAllUsers)
+
 
 module.exports = router;
