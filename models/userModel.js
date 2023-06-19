536a97f6a2fa1d19828fbfd3eb462e08378d14fe
Nutifafa Afi Attor <attorfafa@gmail.com>
2023-06-19 10:24:41 +0000

    added fields to model

--- models/userModel.js
+++ models/userModel.js
@@ -32,7 +32,29 @@ var userSchema = new mongoose.Schema({
     role: {
         type: String,
         default: "user",
-    }
+    },
+    isBlocked: {
+        type: Boolean,
+        default: false,
+    },
+    cart: {
+        type: Array,
+        default: [],
+    },
+    // address: {
+    //     type: String,
+    // },
+    address: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Address",
+    }],
+    wishlist: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+    }],
+
+}, {
+    timestamps: true,
 });
 
 // Encrypt password
