70fc640a14dfddc4cb2e74219468b10e7ba8da24
Nutifafa Afi Attor <attorfafa@gmail.com>
2023-06-19 13:25:57 +0000

    cookie parser

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ const PORT = process.env.PORT || 4000
 const authRouter = require('./routes/authRoute')
 const bodyParser = require('body-parser')
 const { notFound, errorHandler } = require('./middlewares/errorHandler')
+const cookieParser = require('cookie-parser')
 
 dbConnect() // Connecting to the database
 
 
 app.use(bodyParser.json()) // Parsing incoming JSON data
 app.use(bodyParser.urlencoded({ extended: false })); // Parsing incoming form data
+app.use(cookieParser())
 
 // ROUTES
 app.use('/api/user', authRouter) // Registering the authentication router at the specified route
