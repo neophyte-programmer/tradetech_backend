3c538ddbe1ca4f2f020e1f50b7d51ca4f8cdde57
Nutifafa Afi Attor <attorfafa@gmail.com>
2023-06-20 15:08:12 +0000

    sort results

--- controllers/productController.js
+++ controllers/productController.js
@@ -79,6 +79,14 @@ const getAllProducts = asyncHandler(async (req, res) => {
 
         let query = Product.find(JSON.parse(queryStr));
 
+        // Sorting
+        if (req.query.sort) {
+            const sortBy = req.query.sort.split(',').join(' ');
+            query = query.sort(sortBy)
+        } else {
+            query = query.sort('-createdAt')
+        }
+
         const products = await query
         res.json(products)
     } catch (error) {
