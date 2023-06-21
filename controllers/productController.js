145a43436a8e21f2707b62c51bf4708211ba91f3
Nutifafa Afi Attor <attorfafa@gmail.com>
2023-06-21 10:18:44 +0000

    pagination works

--- controllers/productController.js
+++ controllers/productController.js
@@ -96,7 +96,16 @@ const getAllProducts = asyncHandler(async (req, res) => {
         }
 
         // Pagination
-        
+        const page = req.query.page;
+        const limit = req.query.limit
+        const skip = (page - 1) * limit;
+
+        query = query.skip(skip).limit(limit);
+
+        if (req.query.page) {
+            const productCount = await Product.countDocuments();
+            if (skip >= productCount) throw new Error("This Page does not exists");
+          }
 
         const products = await query
         res.json(products)
