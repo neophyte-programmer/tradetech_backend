4b565eb2e1b79b566eaf6123d635228c26d28a41
Nutifafa Afi Attor <attorfafa@gmail.com>
2023-07-08 11:11:02 +0000

    cors again

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const morgan = require('morgan')
 
 const dbConnect = require('./config/dbConnect')
 const { notFound, errorHandler } = require('./middlewares/errorHandler')
-const cors=require("cors");
+const cors = require("cors");
 
 const app = express()
 const PORT = process.env.PORT || 4000
@@ -27,13 +27,12 @@ const couponRouter = require('./routes/couponRoute')
 dbConnect() // Connecting to the database
 
 
-const corsOptions ={
-    origin:'http://localhost:3000', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200,
- }
- 
- app.use(cors(corsOptions)) 
+const corsOptions = {
+    origin: true,
+    credentials: true,
+}
+
+app.use(cors(corsOptions))
 
 app.use(morgan("dev")) // Logging incoming requests
 app.use(bodyParser.json()) // Parsing incoming JSON data
